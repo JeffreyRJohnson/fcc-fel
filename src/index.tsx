3c1a3d53507a030dcbd42100fd4dd5004702b928
Jeffrey Johnson <jeffreyrjohnsonsemail@gmail.com>
2018-07-13 09:38:57 -0500

    added Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import store from "./configureStore";
 import registerServiceWorker from "./registerServiceWorker";
 
 import "./scss/index.css";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root") as HTMLElement
+);
 registerServiceWorker();
